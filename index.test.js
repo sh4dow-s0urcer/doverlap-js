18b94e59194deaab63a7c32d2560ba866da4d491
unknown <axelgrullon@outlook.com>
2021-06-14 15:07:49 -0400

    Testing suite is now ready with two unit tests: doverlap 3.0

--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
 const doverlapCheck = require("./index");
 
-test("Passes the test when two given ranges overlap", () => {
-    expect(doverlapCheck(2,4,4,5)).toBe(false);
+test("Validating paramaters when one argument is missing.", () => {
+    expect(doverlapCheck(2,4,4)).rejects.toEqual(new Error());
+});
+
+test("Validating paramaters when two or more arguments are missing.", () => {
+    expect(doverlapCheck(2)).rejects.toEqual(new Error());
 });
