7f6383ea0452ec866c6c9d0dfac57feb8ca8cfc6
unknown <axelgrullon@outlook.com>
2021-06-15 19:03:30 -0400

    Certain tests were added

--- index.test.js
+++ index.test.js
@@ -1,9 +1,17 @@
 const doverlapCheck = require("./index");
 
-test("Validating paramaters when one argument is missing.", () => {
-    expect(doverlapCheck(2,4,4)).rejects.toEqual(new Error());
+test("Checks when two ranges overlap with one another.", () => {
+    expect(doverlapCheck(2,3,3,4)).toBe(false);
 });
 
 test("Validating paramaters when two or more arguments are missing.", () => {
     expect(doverlapCheck(2)).rejects.toEqual(new Error());
+});
+
+test("Validating paramaters when one argument is missing.", () => {
+    expect(doverlapCheck(2,3,4)).rejects.toEqual(new Error());
+});
+
+test("Checks when two ranges do not overlap with one another.", () => {
+    expect(doverlapCheck(2,3,5,6)).toBe(true);
 });
