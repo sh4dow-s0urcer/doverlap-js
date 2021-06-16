b155715f506a52f63fff18b014ae862fcdf4d0ff
unknown <axelgrullon@outlook.com>
2021-06-15 21:00:49 -0400

    Done

--- index.test.js
+++ index.test.js
@@ -4,12 +4,12 @@ test("Checks when two ranges overlap with one another.", () => {
     expect(doverlapCheck(2,3,3,4)).toBe(false);
 });
 
-test("Validating paramaters when two or more arguments are missing.", () => {
-    expect(doverlapCheck(2)).rejects.toEqual(new Error());
-});
-
 test("Validating paramaters when one argument is missing.", () => {
     expect(doverlapCheck(2,3,4)).rejects.toEqual(new Error());
+});
+
+test("Validating paramaters when two or more arguments are missing.", () => {
+    expect(doverlapCheck(2)).rejects.toEqual(new Error());
 });
 
 test("Checks when two ranges do not overlap with one another.", () => {
